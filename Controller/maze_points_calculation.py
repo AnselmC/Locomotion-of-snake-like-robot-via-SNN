61acad64f00102377776bd6894e48efbfb947bf2
Christoph Clement <christoph.clement@hotmail.de>
2018-06-14 22:50:37 +0200

    small changes

--- Controller/maze_points_calculation.py
+++ Controller/maze_points_calculation.py
@@ -7,15 +7,15 @@ Created on Thu Jun 14 13:41:10 2018
 
 import matplotlib.pylab as plt
 import numpy as np
-from numpy.linalg import norm 
-import math 
+from numpy.linalg import norm
+import math
 
 class maze_points_calculation():
     def __init__(self):
-        
+
         self.length_wall = 10
         self.snake_init_position = [0.0, 0.0]
-        
+
         self.alpha1_deg = 20
         self.alpha1_rad = self.alpha1_deg*math.pi/180
         self.alpha2_deg = 30
@@ -33,7 +33,7 @@ class maze_points_calculation():
         self.length_sin_3 = self.length_wall*math.sin(self.alpha3_rad)
         self.length_cos_4 = self.length_wall*math.cos(self.alpha4_rad)
         self.length_sin_4 = self.length_wall*math.sin(self.alpha4_rad)
-        
+
         self.p00 = self.snake_init_position
         self.p01 = [self.p00[0] + self.length_wall,  self.p00[1]]
         self.p02 = [self.p01[0] + self.length_cos_1, self.p01[1] + self.length_sin_1]
@@ -44,10 +44,9 @@ class maze_points_calculation():
         self.p07 = [self.p06[0] + self.length_wall,  self.p06[1]]
         self.p08 = [self.p07[0] + self.length_cos_4, self.p07[1] - self.length_sin_4]
         self.p09 = [self.p08[0] + self.length_wall,  self.p08[1]]
-        
-        self.points = [self.p00, self.p01, self.p02, self.p03, self.p04, self.p05, self.p06, self.p07, self.p08, self.p09]
-        
-maze_points_calculation = maze_points_calculation()
-    
-points = maze_points_calculation.points
 
+        self.points = [self.p00, self.p01, self.p02, self.p03, self.p04, self.p05, self.p06, self.p07, self.p08, self.p09]
+
+maze_points_calculation = maze_points_calculation()
+
+print maze_points_calculation.points
