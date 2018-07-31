6c9d06db4ca24f6988e82cd2ea5c68e993f12065
Christoph Clement <christoph.clement@hotmail.de>
2018-07-31 12:24:36 +0200

    Added performance parameters

--- Controller/controller.py
+++ Controller/controller.py
@@ -19,6 +19,9 @@ snn.set_weights(w_l, w_r)
 
 distances = []
 positions = []
+rewards = []
+steps = []
+terminate_positions = []
 
 # Initialize environment, get state, get reward
 state, reward = env.reset()
@@ -39,9 +42,17 @@ for i in range(50000):
     # Store position, distance
     distances.append(distance)
     positions.append(pos_data)
+    rewards.append(reward)
+    steps.append(step)
+    terminate_positions.append(terminate_position)
 
 # Save performance data
 h5f = h5py.File(path + '/rstdp_performance_data.h5', 'w')
-h5f.create_dataset('distance', data=distances)
-h5f.create_dataset('position', data=positions)
+h5f.create_dataset('distances', data=distances)
+h5f.create_dataset('positions', data=positions)
+h5f.create_dataset('rewards', data=rewards)
+h5f.create_dataset('steps', data=steps)
+h5f.create_dataset('terminate_positions', data=terminate_positions)
+h5f.create_dataset('travelled_distances', data=travelled_distances)
+h5f.create_dataset('vrep_steps', data=vrep_steps)
 h5f.close()
