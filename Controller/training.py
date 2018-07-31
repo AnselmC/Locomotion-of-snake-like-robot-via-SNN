3e25bf8712b4509dd67f630d047a2e522b46569b
Christoph Clement <christoph.clement@hotmail.de>
2018-07-31 12:29:55 +0200

    Changed imports

--- Controller/training.py
+++ Controller/training.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-from network import *
-from environment import *
-import parameters as p
 import h5py
 import json
 import signal
 
-snn = SpikingNeuralNetwork()
-env = VrepEnvironment()
+import network as net
+import environment as env
+import parameters as params
+
+snn = net.SpikingNeuralNetwork()
+env = env.VrepEnvironment()
 
 weights_r = []
 weights_l = []
@@ -29,16 +30,16 @@ signal.signal(signal.SIGINT, handler)
 # Initialize environment, get initial state, initial reward
 state, reward = env.reset()
 
-for i in range(p.training_length):
+for i in range(params.training_length):
 
     # Simulate network for 50 ms
-    # get number of output spikes and network weights
+    # Get number of output spikes and network weights
     n_l, n_r, weights = snn.simulate(state, reward)
     w_l = weights[0]
     w_r = weights[1]
 
-    # Feed output spikes in model
-    # Get state, distance, pos_data, reward, t, step
+    # Get state, distance, pos_data, reward, terminate, steps,
+    # terminate_position, travelled_distances, vrep_steps
     (state, distance, pos_data, reward, t, step,
      terminate_position, travelled_distances, vrep_steps) = env.step(n_l, n_r)
 
@@ -59,7 +60,7 @@ for i in range(p.training_length):
         weights_r.append(w_r)
         weights_i.append(i)
 
-    # Save no. of steps every episode
+    # Save # steps every episode
     if t:
         steps.append(step)
         terminate_positions.append(terminate_position)
@@ -76,19 +77,19 @@ for i in range(p.training_length):
 # Save training parameters
 try:
     print "saving params"
-    params = p.params_dict
+    params = params.params_dict
     print params
     # Save to single json file
     json_data = json.dumps(params, indent=4, sort_keys=True)
     print "converted to json"
-    with open(p.path+'/training_parameters.json','w') as file:
+    with open(params.path+'/training_parameters.json','w') as file:
         file.write(json_data)
 except:
     print "saving params failed"
     pass
 
 # Save data
-h5f = h5py.File(p.path + '/rstdp_data.h5', 'w')
+h5f = h5py.File(params.path + '/rstdp_data.h5', 'w')
 h5f.create_dataset('w_l', data=weights_l)
 h5f.create_dataset('w_r', data=weights_r)
 h5f.create_dataset('w_i', data=weights_i)
